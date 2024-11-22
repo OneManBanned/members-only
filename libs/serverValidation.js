e1095106904c84ce4a5a47d5fd0b68d212470482
Brendan <brendanlawless@hotmail.com>
2024-11-22 18:35:02 +0000

    feat[signUp]: add server validation schema

---
+++ libs/serverValidation.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+
+export const signUpValidation = [
+  body(["firstname", "lastname"])
+    .trim()
+    .notEmpty()
+    .withMessage("must not be empty")
+    .isLength({ max: 255 })
+    .withMessage("must contain fewer than 255 characters"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("must not be empty")
+    .isLength({ max: 255 })
+    .withMessage("must contain fewer than 255 characters")
+    .isEmail()
+    .withMessage("must be a valid email"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("must not be empty")
+    .isLength({ max: 255})
+    .withMessage("must contain fewer than 255 characters")
+    .isLength({ min: 8})
+    .withMessage("must contain atleast 8 characters"),
+  body("confirmPassword")
+    .trim()
+    .notEmpty()
+    .withMessage("must not be empty")
+    .isLength({ max: 255})
+    .withMessage("must contain fewer than 255 characters")
+    .isLength({ min: 8 })
+    .withMessage("must contain atleast 8 characters")
+    .custom((pw, {req}) => pw === req.body.password)
+    .withMessage("passwords must match"),
+];
