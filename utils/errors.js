a1b2504594ee14513e2b9c54c295ee9410c408d4
Brendan <brendanlawless@hotmail.com>
2024-11-26 19:40:10 +0000

    fix: add missing custom errors and updated index.html for delete

--- utils/errors.js
+++ utils/errors.js
@@ -1,7 +1,15 @@
 export class CustomNotFoundError extends Error {
     constructor(message) {
         super(message);
-        this.statusCode(404)
+        this.statusCode = 404;
         this.name = "NotFoundError";
+    }
+}
+
+export class CustomBadRequestError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = 400;
+        this.name = "BadRequest";
     }
 }
